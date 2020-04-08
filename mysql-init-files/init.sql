932bfd1ef0bd0436fb6bba16a2542419e8626833
Sammy Barasa <barsamms@gmail.com>
2020-04-08 17:37:03 +0300

    Added all traefik configs

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,5 +1,6 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `fuelrod`;
+CREATE DATABASE IF NOT EXISTS `tsobu_site`;
 
 # create root user and grant rights
 GRANT ALL PRIVILEGES ON fuelrod.* TO 'fuelrod'@'%';
