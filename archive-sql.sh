a667d6a53d9496306ee9ac584d4dd0cabdf7b531
Tsobu Vanguard <dev@tsobu.co.ke>
2020-06-25 02:55:24 +0000

    Updates

--- archive-sql.sh
+++ archive-sql.sh
@@ -2,4 +2,4 @@
 
 timestamp=$(date +%Y%m%d%H%M%S)
 
-zip -r "${timestamp}_backups.zip" *.sql && rm *.sql
+zip -r "${timestamp}_backups.zip" *.sql && rm *.sql && mv "${timestamp}_backups.zip" db-backup/
