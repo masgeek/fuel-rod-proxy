b724a5b507ef1f8042521683a9ff1ee0455804d6
Tsobu Vanguard <dev@tsobu.co.ke>
2020-07-08 08:25:25 +0000

    Updatebd db backup file

--- database-backup.sh
+++ database-backup.sh
@@ -9,3 +9,5 @@ timestamp=$(date +%Y%m%d%H%M%S)
 filename="${timestamp}-${DATABASE}.sql"
 
 docker exec db /usr/bin/mysqldump -u "${DATABASE_USERNAME}" --password="${DATABASE_PASSWORD}" "${DATABASE}" >"$filename"
+
+sed -i "$filename" -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
