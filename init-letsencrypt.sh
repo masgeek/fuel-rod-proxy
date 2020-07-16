d6feea2badd6cd8566c196c736dac4f930ece26c
Sammy Barasa <barsamms@gmail.com>
2020-07-16 14:25:59 +0300

    Updates

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -11,7 +11,7 @@ domains=(tsobu.co.ke www.tsobu.co.ke api.tsobu.co.ke)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="barsamms@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
