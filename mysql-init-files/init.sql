9dae4defe5362d86b0e695b576bbcd228d8d61fc
Sammy Barasa <barsamms@gmail.com>
2021-08-16 22:46:30 +0300

    refactor: env values

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,7 +1,5 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `fuelrod`;
 
-CREATE DATABASE IF NOT EXISTS `blog`;
-
 # create root user and grant rights
 GRANT ALL PRIVILEGES ON fuelrod.* TO 'fuelrod'@'%';
