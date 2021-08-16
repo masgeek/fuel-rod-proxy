4524935052adff4289af723b799de9ece37b6e78
Sammy Barasa <barsamms@gmail.com>
2021-08-16 20:56:01 +0300

    feat: added new blogging container

--- mysql-init-files/init.sql
+++ mysql-init-files/init.sql
@@ -1,5 +1,7 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `fuelrod`;
 
+CREATE DATABASE IF NOT EXISTS `database`;
+
 # create root user and grant rights
 GRANT ALL PRIVILEGES ON fuelrod.* TO 'fuelrod'@'%';
