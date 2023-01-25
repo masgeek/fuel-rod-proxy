4946d33611d7806838d63ab121e7f4542eeacb05
Sammy M <barsamms@gmail.com>
2023-01-25 09:41:22 +0300

    Added verbosity

--- database-backup.sh
+++ database-backup.sh
@@ -54,7 +54,8 @@ do
 
         sed -i "${filename}" -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
         # move to working dir
-        mv "${dir}/${filename}"
+        echo "Moving file to "${dir}/${filename}""
+        mv "${filename}" "${dir}/db-backup"
 		;;
   esac
 done;
