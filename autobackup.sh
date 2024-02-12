4d0c882ff61afcf04e1b49a8ab2891560ddde512
Sammy Barasa <barsamms@gmail.com>
2024-02-12 15:07:06 +0300

    WIP

--- autobackup.sh
+++ autobackup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-
 dir="$(dirname "$(realpath "$0")")"
 
 echo "Directory is ${dir}"
 
 # Call database-backup.sh and pass arguments
 "${dir}/database-backup.sh" "$@" &&
-# Call gbk.sh with the size parameter, if provided
-if [[ $@ =~ "--size" ]]; then
-    "${dir}/archive-sql.sh"  "$@" &&
+
+# Check if the "--size" argument is provided
+if [[ "$@" =~ "--size" ]]; then
+    "${dir}/archive-sql.sh" "$@" &&
 else
- "${dir}/archive-sql.sh" &&
+    "${dir}/archive-sql.sh" &&
 fi
+
 # Call gbk.sh without arguments
 "${dir}/gbk.sh"
