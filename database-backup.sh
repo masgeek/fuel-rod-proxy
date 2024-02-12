0a406b358ea32d5cfc648155a5d333af57314c0c
Sammy Barasa <barsamms@gmail.com>
2024-02-12 11:41:07 +0300

    refactor: added dynamic db connection command

--- database-backup.sh
+++ database-backup.sh
@@ -40,9 +40,11 @@ echo "Directory is ${dir}"
 
 # Determine if the database is MariaDB or MySQL
 dbType=$(docker exec "${dbService}" mysql -V | grep -o -E "MariaDB|MySQL")
+dbRunner="mysql"
 
 if [[ "$dbType" == "MariaDB" ]]; then
   dumpCommand="mariadb-dump"
+  dbRunner="maria"
 elif [[ "$dbType" == "MySQL" ]]; then
   dumpCommand="mysqldump"
 else
@@ -53,7 +55,7 @@ fi
 echo "Running dump with $dbType"
 
 # Iterate over each schema in the database
-for T in $(docker exec "${dbService}" mysql -u "${dbUser}" --password="${dbPass}" -h "${dbHost}" -N -B -e 'SHOW schemas;'); do
+for T in $(docker exec "${dbService}" "${dbRunner}" -u "${dbUser}" --password="${dbPass}" -h "${dbHost}" -N -B -e 'SHOW schemas;'); do
   case $T in
     information_schema|mysql|performance_schema|sys|test)
       echo "Skipping backup of $T schema"
