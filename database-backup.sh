b0fa680f9e0da12264fa0a295e5253ea95436128
Sammy Barasa <barsamms@gmail.com>
2024-02-12 11:38:08 +0300

    feat: revised database-backup.sh for improved flow and clarity

--- database-backup.sh
+++ database-backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-#set -o allexport; source /home/akilimo/services/tsobu-proxy/.env; set +o allexport
+# Uncomment the following line if you want to set all environment variables from .env file
+# set -o allexport; source /home/akilimo/services/tsobu-proxy/.env; set +o allexport
 
 while [ $# -gt 0 ]; do
   case "$1" in
@@ -19,7 +19,7 @@ while [ $# -gt 0 ]; do
       ;;
     *)
       printf "***************************\n"
-      printf "* Error: Invalid argument.*\n"
+      printf "* Error: Invalid argument. *\n"
       printf "***************************\n"
       exit 1
   esac
@@ -29,33 +29,51 @@ done
 
 timestamp=$(date +%Y_%d%b_%H%M)
 
-
-dbUser="${user:-akilimo}"
-dbPass="${pass-andalite6}"
+# Default values if not provided
+dbUser="${user:-backup_user}"
+dbPass="${pass:-andalite6}"
 dbService="${service:-maria}"
 dbHost="${host:-127.0.0.1}"
 
 dir="$(dirname "$(realpath "$0")")"
-
-
 echo "Directory is ${dir}"
 
-for T in `docker exec ${dbService} mysql -u ${dbUser} --password=${dbPass} -h ${dbHost} -N -B -e 'SHOW schemas;'`;
-do
+# Determine if the database is MariaDB or MySQL
+dbType=$(docker exec "${dbService}" mysql -V | grep -o -E "MariaDB|MySQL")
 
+if [[ "$dbType" == "MariaDB" ]]; then
+  dumpCommand="mariadb-dump"
+elif [[ "$dbType" == "MySQL" ]]; then
+  dumpCommand="mysqldump"
+else
+  echo "Error: Unsupported database type."
+  exit 1
+fi
+
+echo "Running dump with $dbType"
+
+# Iterate over each schema in the database
+for T in $(docker exec "${dbService}" mysql -u "${dbUser}" --password="${dbPass}" -h "${dbHost}" -N -B -e 'SHOW schemas;'); do
   case $T in
-	information_schema|mysql|performance_schema|sys|test)
-    echo "Skip backing up of $T schema"
-		;;
-	*)
-        filename="${timestamp}_${T}.sql"
-        echo "Backing up $T to file name ${filename}"
-        docker exec "${dbService}" mysqldump --no-tablespaces -u "${dbUser}" --password="${dbPass}" $T > $filename
+    information_schema|mysql|performance_schema|sys|test)
+      echo "Skipping backup of $T schema"
+      ;;
+    *)
+      filename="${timestamp}_${T}.sql"
+      nodataFileName="${timestamp}_${T}_structure.sql"
+      echo "Dumping $T with data to file name ${filename}"
+      docker exec "${dbService}" "$dumpCommand" --verbose --no-tablespaces -u "${dbUser}" --password="${dbPass}" "$T" > "$filename"
 
-        sed -i "${filename}" -e 's/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g'
-        # move to working dir
-        echo "Moving file to ${filename} "${dir}/db-backup/${filename}""
-        mv "${filename}" "${dir}/db-backup"
-		;;
+      echo "Dumping $T with no data to file name ${nodataFileName}"
+      docker exec "${dbService}" "$dumpCommand" --verbose --no-tablespaces --no-data -u "${dbUser}" --password="${dbPass}" "$T" > "$nodataFileName"
+
+      # Replace charset in the dump file
+      sed -i "s/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g" "$filename"
+
+      # Move dump files to the backup directory
+      echo "Moving file to ${filename} in ${dir}/db-backup/"
+      mv "$filename" "${dir}/db-backup/"
+      mv "$nodataFileName" "${dir}/db-backup/"
+      ;;
   esac
-done;
+done
