4c72dfbf2695d8e279f4136a938d202527dc87bd
Sammy Barasa <barsamms@gmail.com>
2024-02-12 12:31:43 +0300

    feat: revised gbk.sh script

--- gbk.sh
+++ gbk.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
+# Set default values
+backupDir="db-backup"
+
+# Parse command-line arguments
 while [ $# -gt 0 ]; do
   case "$1" in
     -g|-gdrive|--gdrive)
       gdrive="$2"
+      shift
       ;;
     *)
       printf "***************************\n"
-      printf "* Error: Invalid argument.*\n"
+      printf "* Error: Invalid argument. *\n"
       printf "***************************\n"
       exit 1
   esac
   shift
-  shift
 done
 
+# Get the directory of the script
 dir="$(dirname "$(realpath "$0")")"
-backupDir="${gdrive:-db-backup}"
 
-echo "Directory is ${dir} backing up to ${backupDir} on google drive"
+echo "Directory is ${dir}, backing up to ${backupDir} on Google Drive"
 
-rclone move --update --include "*sql.zip" --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 "${dir}/db-backup/" "gdrive:${backupDir}"
+# Move SQL files to Google Drive
+rclone move --update --include "*.sql.zip" --verbose --transfers 30 --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 "${dir}/${backupDir}/" "gdrive:${gdrive}/${backupDir}"
 
 echo "Clearing remote directory"
 
-rclone --drive-use-trash=false --verbose --min-age 2d delete gdrive:db-backup
+# Delete old files from Google Drive
+rclone --drive-use-trash=false --verbose --min-age 2d delete "gdrive:${gdrive}/${backupDir}"
+
+# rclone --drive-use-trash=false --verbose --min-age 2d delete gdrive:db-backup
 
 #rm "${dir}/db-backup/*"
