3c5ff34ccf326a2b1ccf2087e414df27a36188f3
Sammy Barasa <barsamms@gmail.com>
2024-02-12 15:18:59 +0300

    WIP

--- archive-sql.sh
+++ archive-sql.sh
@@ -1,44 +1,14 @@
 #!/bin/bash
 
-# Default size threshold (20KB)
-DEFAULT_SIZE_THRESHOLD="20k"
+#timestamp=$(date +%Y%m%d%H)
 
-# Function to display usage information
-usage() {
-    echo "Usage: $(basename "$0") [-s|--size <size>] [--help]"
-    echo "  -s, --size <size>   Specify the size threshold for files to be archived (default: 20k)"
-    echo "  --help              Display this help message"
-    exit 1
-}
+#zip -r "${timestamp}_backups.zip" *.sql && rm *.sql && mv "${timestamp}_backups.zip" /home/akilimo/services/tsobu-proxy/db-backup/
 
-# Parse command-line arguments
-while [[ $# -gt 0 ]]; do
-    case "$1" in
-        -s|--size)
-            size_threshold="$2"
-            shift 2
-            ;;
-        --help)
-            usage
-            ;;
-        *)
-            echo "Invalid option: $1" >&2
-            shift
-            ;;
-    esac
-done
-
-# Set size threshold or default value if not provided
-size_threshold="${size_threshold:-$DEFAULT_SIZE_THRESHOLD}"
-
-# Get the directory of the script
 dir="$(dirname "$(realpath "$0")")"
+
+
 echo "Directory is ${dir}"
 
-# Archive files greater than the size threshold
-find "${dir}/db-backup" -name '*.sql' -print -size +${size_threshold} -exec zip -r -j '{}'.zip '{}' \; -exec rm '{}' \;
+#find "${dir}/db-backup" -name '*.sql' -print -exec zip '{}'.zip '{}' \; -exec rm '{}' \; -exec mv '{}'.zip "${dir}/db-backup" \;
 
-# Delete files that do not meet the size threshold
-find "${dir}/db-backup" -name '*.sql' ! -size +${size_threshold} -delete
-
-exit 0
+find "${dir}/db-backup" -name '*.sql' -print -exec zip -r -j '{}'.zip '{}' \; -exec rm '{}' \;
