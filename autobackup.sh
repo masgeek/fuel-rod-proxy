2e9c04c3e02d7d7640b229b99bdf71b60dadf5ba
Sammy Barasa <barsamms@gmail.com>
2024-02-12 13:17:28 +0300

    feat: added arguments

--- autobackup.sh
+++ autobackup.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Function to display usage information
+usage() {
+    echo "Usage: $(basename "$0") [-s|--size <size>] [--help]"
+    echo "  -s, --size <size>   Specify the size threshold for files to be archived"
+    echo "  --help              Display this help message"
+    exit 1
+}
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -s|--size)
+            size_threshold="$2"
+            shift 2
+            ;;
+        --help)
+            usage
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            usage
+            ;;
+    esac
+done
+
 dir="$(dirname "$(realpath "$0")")"
 
 echo "Directory is ${dir}"
@@ -7,6 +31,6 @@ echo "Directory is ${dir}"
 # Call database-backup.sh and pass arguments
 "${dir}/database-backup.sh" "$@" &&
 # Call archive-sql.sh without arguments
-"${dir}/archive-sql.sh" "$@" &&
+"${dir}/archive-sql.sh" --size 200k &&
 # Call gbk.sh without arguments
 "${dir}/gbk.sh"
