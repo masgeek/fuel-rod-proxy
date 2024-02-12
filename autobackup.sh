19784685b6a08556a56416e6db0a23e5f89624cb
Sammy Barasa <barsamms@gmail.com>
2024-02-12 13:07:04 +0300

    refactor: added argument carriage

--- autobackup.sh
+++ autobackup.sh
@@ -7,6 +7,6 @@ echo "Directory is ${dir}"
 # Call database-backup.sh and pass arguments
 "${dir}/database-backup.sh" "$@" &&
 # Call archive-sql.sh without arguments
-"${dir}/archive-sql.sh" &&
+"${dir}/archive-sql.sh" "$@" &&
 # Call gbk.sh without arguments
 "${dir}/gbk.sh"
