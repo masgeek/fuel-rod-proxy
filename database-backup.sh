a8b44a286bc9933ff95b5481a620f8c55f4d341d
Sammy Barasa <barsamms@gmail.com>
2024-02-27 10:28:00 +0300

    refactor: revised database backup

--- database-backup.sh
+++ database-backup.sh
@@ -1,102 +1,146 @@
 #!/bin/bash
 
-# Uncomment the following line if you want to set all environment variables from .env file
-# set -o allexport; source /home/akilimo/services/tsobu-proxy/.env; set +o allexport
-# Load environment variables from .env file if present
+# Function to log messages
+log() {
+    local message="$1"
+    local timestamp=$(date +'%Y-%m-%d %H:%M:%S')
+    echo "[$timestamp] $message" >> /var/log/db_backup.log
+}
+
+# Function to send telemetry on errors
+send_telemetry() {
+    local error_message="$1"
+    log "Sending telemetry on error: $error_message"
+    curl -X POST -s " https://cronitor.link/p/b5cbdedf915c4a22be135d4ae6d883c1/ya2G2O/fail"
+}
+
+# Function to handle errors
+handle_error() {
+    local error_message="$1"
+    log "ERROR: $error_message"
+    echo "ERROR: $error_message"
+    # Add notification mechanism here (e.g., send email, notify via Slack)
+    send_telemetry "$error_message"
+    exit 1
+}
+
+# Load environment variables from .backup file if present
 dir="$(dirname "$(realpath "$0")")"
-
 if [[ -f "$dir/.backup" ]]; then
-  export $(grep -v '^#'  $dir/.backup | xargs)
-  echo "Exported environment variables"
+    export $(grep -v '^#' "$dir/.backup" | xargs)
+    log "Exported environment variables"
 fi
 
+# Process command-line arguments
 while [ $# -gt 0 ]; do
-  case "$1" in
-    -u|-user|--user)
-      user="$2"
-      shift 2
-      ;;
-    -p|-pass|--pass)
-      pass="$2"
-      shift 2
-      ;;
-    -s|-service|--service)
-      service="$2"
-      shift 2
-      ;;
-    -h|-host|--host)
-      host="$2"
-      shift 2
-      ;;
-    -t|-type|--type)
-      dbType="$2"
-      shift 2
-      ;;
-    *)
-      printf "***************************\n"
-      printf "* Error: Invalid argument. *\n"
-      printf "***************************\n"
-      shift 2  # Skip the invalid argument and its value
-      ;;
-  esac
+    case "$1" in
+        -u|-user|--user)
+            shift
+            user="$1"
+            ;;
+        -p|-pass|--pass)
+            shift
+            pass="$1"
+            ;;
+        -s|-service|--service)
+            shift
+            service="$1"
+            ;;
+        -h|-host|--host)
+            shift
+            host="$1"
+            ;;
+        -t|-type|--type)
+            shift
+            dbType="$1"
+            ;;
+        -b|-backup|--backup)
+            shift
+            backup_type="$1"
+            ;;
+        *)
+            handle_error "Invalid argument: $1"
+            ;;
+    esac
+    shift
 done
 
+# Set default values
+user="${user:-backup_user}"
+service="${service:-maria}"
+host="${host:-127.0.0.1}"
+dbType="${dbType:-MariaDB}"  # Default to MariaDB if not provided
+backup_type="${backup_type:-full}" # Default to full backup
 
+# Validate input parameters
+if [[ -z "$user" || -z "$pass" ]]; then
+    handle_error "Username or password not provided"
+fi
+
+# Validate database type
+case "${dbType,,}" in
+    mariadb|mysql)
+        ;;
+    *)
+        handle_error "Unsupported database type: $dbType"
+        ;;
+esac
+
+# Validate backup type
+case "${backup_type,,}" in
+    full|incremental)
+        ;;
+    *)
+        handle_error "Unsupported backup type: $backup_type"
+        ;;
+esac
+
+# Perform backup
 timestamp=$(date +%Y_%d%b_%H%M)
+backup_dir="${dir}/db-backup"
+mkdir -p "$backup_dir" || handle_error "Failed to create backup directory: $backup_dir"
 
-# Default values if not provided
-dbUser="${user:-backup_user}"
-dbPass=$DB_PASS
-dbService="${service:-maria}"
-dbHost="${host:-127.0.0.1}"
-dbType="${dbType:-MariaDB}" # Default to MariaDB if not provide
-dbType=$(echo "$dbType" | tr '[:upper:]' '[:lower:]') # Convert to lowercase
+log "Starting $backup_type database backup for $dbType at $timestamp"
 
-echo "Directory is ${dir} and password is $DB_PASS"
-
-# Determine if the database is MariaDB or MySQL
-dbRunner="mariadb"
-if [[ "$dbType" == "mariadb" ]]; then
-  dumpCommand="mariadb-dump"
-  dbRunner="mariadb"
-elif [[ "$dbType" == "mysql" ]]; then
-  dumpCommand="mysqldump"
-  dbRunner="mysql"
-else
-  echo "Error: Unsupported database type --> ${dbType}"
-  exit 1
+# Run backup command based on database type and backup type
+if [[ "${dbType,,}" == "mariadb" ]]; then
+    dump_command="mariadb-dump"
+    db_runner="mariadb"
+elif [[ "${dbType,,}" == "mysql" ]]; then
+    dump_command="mysqldump"
+    db_runner="mysql"
 fi
 
-echo "Running dump with ${dbType} and DB runner ${dbRunner}"
-
-# Check if connection is successful
-if ! docker exec "${dbService}" "${dbRunner}" -u "${dbUser}" --password="${dbPass}" -h "${dbHost}" -N -B -e 'SHOW schemas;' &>/dev/null; then
-  echo "Error: Unable to connect to the database."
-  exit 1
+# Check database connection
+if ! docker exec "${service}" "${db_runner}" -u "${user}" --password="${pass}" -h "${host}" -N -B -e 'SHOW schemas;' &>/dev/null; then
+    handle_error "Unable to connect to the database"
 fi
 
-# Iterate over each schema in the database
-for T in $(docker exec "${dbService}" "${dbRunner}" -u "${dbUser}" --password="${dbPass}" -h "${dbHost}" -N -B -e 'SHOW schemas;'); do
-  case $T in
-    information_schema|mysql|performance_schema|sys|test)
-      echo "Skipping backup of $T schema"
-      ;;
-    *)
-      filename="${timestamp}_${T}.sql"
-      nodataFileName="${timestamp}_${T}_structure.sql"
-      echo "Dumping $T with data to file name ${filename}"
-      docker exec "${dbService}" "$dumpCommand" --verbose --no-tablespaces -u "${dbUser}" --password="${dbPass}" "$T" > "$filename"
+# Backup each schema
+schemas=$(docker exec "${service}" "${db_runner}" -u "${user}" --password="${pass}" -h "${host}" -N -B -e 'SHOW schemas;')
+while IFS= read -r schema; do
+    case $schema in
+        information_schema|mysql|performance_schema|sys|test)
+            log "Skipping backup of $schema schema"
+            ;;
+        *)
+            filename="${timestamp}_${schema}.sql"
+            log "Dumping $schema with data to file: $filename"
+            if [[ "${backup_type,,}" == "full" ]]; then
+                docker exec "${service}" "$dump_command" --verbose --no-tablespaces -u "${user}" --password="${pass}" "$schema" > "${backup_dir}/${filename}"
+            elif [[ "${backup_type,,}" == "incremental" ]]; then
+                docker exec "${service}" "$dump_command" --verbose --no-tablespaces --incremental -u "${user}" --password="${pass}" "$schema" > "${backup_dir}/${filename}"
+            fi
 
-      # echo "Dumping $T with no data to file name ${nodataFileName}"
-      # docker exec "${dbService}" "$dumpCommand" --verbose --no-tablespaces --no-data -u "${dbUser}" --password="${dbPass}" "$T" > "$nodataFileName"
+            # Add error handling for backup command
+            if [[ $? -ne 0 ]]; then
+                handle_error "Failed to dump $schema"
+            fi
 
-      # Replace charset in the dump file
-      sed -i "s/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g" "$filename"
+            # Replace charset in the dump file
+            sed -i "s/utf8mb4_0900_ai_ci/utf8mb4_unicode_ci/g" "${backup_dir}/${filename}"
+            ;;
+    esac
+done <<< "$schemas"
 
-      # Move dump files to the backup directory
-      echo "Moving file to ${filename} in ${dir}/db-backup/"
-      mv "$filename" "${dir}/db-backup/"
-      # mv "$nodataFileName" "${dir}/db-backup/"
-      ;;
-  esac
-done
+log "Database backup completed successfully"
